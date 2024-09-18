2ada4de74494253d7664dbb2e9a7b5b0f396e4af
Piya Samyak (IT RDI) <Samyak.Piya@infineon.com>
2024-09-18 13:03:34 -0400

    Add basic header and footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,42 @@
+import Link from "next/link";
 import React from "react";
 
+import { Laptop } from "lucide-react";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center">
+      <Link className="flex items-center justify-center" href="/">
+        <span className="sr-only">IntervuPrep</span>
+        <Laptop className="h-6 w-6" />
+        <span className="ml-2 text-2xl font-bold">IntervuPrep</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          Features
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          Pricing
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          About
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
 }
